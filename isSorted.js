5412934b364b6e66077c1adb298b405ed4ef1d31
Piotr Podgorski <p.t.podgorski@gmail.com>
2021-06-12 19:18:23 +0200

    tylko jedna pętla for, dorobine testy

--- isSorted.js
+++ isSorted.js
@@ -6,30 +6,49 @@ isSorted([3, 2, 1]) -> true
 isSorted([1, 1, 10]) -> true
 isSorted([1, 1, 1]) -> true
 isSorted([1, 2, 1]) -> false
+
+I bym se zrobił na Twoim miejscu na końcu tablice obiektow typu 
+const tests = [
+   { array: [1, 2, 3], expectedResult: true },
+   { array: [1, 2, 1], expectedResult: false }
+]
 */
 
 function isSorted(array) {
-  let isSorted = false;
   let isIncreasing = true;
   let isDecreasing = true;
 
   for (let i = 1; i < array.length; i++) {
-    if (!(array[i - 1] === array[i] || array[i - 1] < array[i])) {
+    if (array[i - 1] > array[i]) {
       isIncreasing = false;
     }
-  }
-
-  for (let i = 1; i < array.length; i++) {
-    if (!(array[i - 1] === array[i] || array[i - 1] > array[i])) {
+    if (array[i - 1] < array[i]) {
       isDecreasing = false;
+    }
+    if (isIncreasing === false && isDecreasing === false) {
+      return false;
     }
   }
 
   if (isIncreasing === true || isDecreasing === true) {
-    isSorted = true;
+    return true;
   }
-
-  console.log(isSorted);
 }
 
-isSorted([1, 8, 3, 4]);
+const tests = [
+  { array: [1, 2, 3], expectedResult: true },
+  { array: [3, 2, 1], expectedResult: true },
+  { array: [1, 2, 10], expectedResult: true },
+  { array: [1, 2, 1], expectedResult: false },
+];
+
+function test(tests) {
+  for (let i = 0; i < tests.length; i++) {
+    if (isSorted(tests[i].array) != tests[i].expectedResult) {
+      return `test failed at ${i}-th element`;
+    }
+  }
+  return "test succesful";
+}
+
+console.log(test(tests));
